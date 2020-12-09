4b853014d75f4a0fcac6e3e96eff97d0843b351c
TomSachot <sach.tom02@gmail.com>
2020-12-09 16:15:09 +0100

    Update Exam_python_09-12.py

--- Exam_python_09-12.py
+++ Exam_python_09-12.py
@@ -1,4 +1,8 @@
-tour = 0
+ from colorama import init
+ init()
+ from colorama import Fore, Back, Style
+ import random
+
 mot1 = ["V", "O", "I", "T", "U", "R", "E"]
 mot2 = ["M", "O", "T", "E", "U", "R"]
 mot3 = ["F", "U", "S", "I", "L"]
@@ -9,6 +13,7 @@ mot7 = ["M", "A", "N", "O", "I", "R"]
 mot8 = ["G", "A", "D", "J", "E", "T"]
 mot9 = ["D", "Y", "N", "A", "M", "I", "T", "E"]
 mot10 = ["P", "O", "U", "R", "S", "U", "I", "T", "E"]
+tour = 0
 motmystere = 
 motjoueur = int(input("Quel est le mot mystère ?")
 
@@ -22,4 +27,10 @@ while (tour < 8):
         if (motjoueur = motmystere):
             print("Bien joué ! C'est bien ",motjoueur," le mot mystère !")
         if (motjoueur != motmystere):
-            
+            for i in range(1, 9):
+                if (tableau[i] = )
+                    print(Back.RED
+                if (tableau[i] != )
+                    print(Back.BLUE
+                if (tableau[i] =  )
+                    print(Back.YELLOW
