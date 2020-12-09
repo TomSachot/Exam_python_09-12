2f87e46f1fc2b646462c471769d63c1ba9b98196
TomSachot <sach.tom02@gmail.com>
2020-12-09 15:12:25 +0100

    Update Exam_python_09-12.py

--- Exam_python_09-12.py
+++ Exam_python_09-12.py
@@ -1,4 +1,11 @@
 tour = 0
-tableau = [1, 2, 3, 4, 5, 6, 7, 8]
-mot =
-lettre =
+mot1 = [v, o, i, t, u, r, e]
+mot2 = [f, o, o, t, b, a, l, l]
+mot3 = [r, o, u, t, e]
+mot4 = [p, e, l, o, u, s, e]
+mot5 = [b, o, u, l, e, v, a, r, d]
+mot6 = [e, c, o, s, s, e]
+mot7 = [m, a, n, o, i, r]
+mot8 = [s, o, u, t, e, r, r, a, i, n]
+mot9 = [s, p, e, c, t, r, e]
+mot10 = [c, a, s, c, a, d, e]
