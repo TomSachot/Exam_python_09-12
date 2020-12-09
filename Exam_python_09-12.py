816c43d37e67bd567482e332c1045f248a40a416
TomSachot <sach.tom02@gmail.com>
2020-12-09 15:20:36 +0100

    Update Exam_python_09-12.py

--- Exam_python_09-12.py
+++ Exam_python_09-12.py
@@ -1,14 +1,14 @@
 tour = 0
-mot1 = [v, o, i, t, u, r, e]
-mot2 = [f, o, o, t, b, a, l, l]
-mot3 = [a, t, t, e, n, t, a, t]
-mot4 = [p, e, l, o, u, s, e]
-mot5 = [b, o, u, l, e, v, a, r, d]
-mot6 = [e, c, o, s, s, e]
-mot7 = [m, a, n, o, i, r]
-mot8 = [s, o, u, t, e, r, r, a, i, n]
-mot9 = [s, p, e, c, t, r, e]
-mot10 = [c, a, s, c, a, d, e]
+mot1 = ["v", "o", "i", "t", "u", "r", "e"]
+mot2 = ["f", "o", "o", "t", "b", "a", "l", "l"]
+mot3 = ["a", "t", "t", "e", "n", "t", "a", "t"]
+mot4 = ["p", "e", "l", "o", "u", "s", "e"]
+mot5 = ["b", "o", "u", "l", "e", "v", "a", "r", "d"]
+mot6 = ["e", "c", "o", "s", "s", "e"]
+mot7 = ["m", "a", "n", "o", "i", "r"]
+mot8 = ["s", "o", "u", "t", "e", "r", "r", "a", "i", "n"]
+mot9 = ["s", "p", "e", "c", "t", "r", "e"]
+mot10 = ["c", "a", "s", "c", "a", "d", "e"]
 motordi = 0
 
 for motchoisi in range(mot1, mot10):
